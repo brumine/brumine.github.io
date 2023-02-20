c3f400d57aca1e4a4e04dc051ca81aa3fbcd85f8
Marcus Bruno Gonçalves Maria <marcusbrunogm@outlook.com>
2023-02-20 12:05:29 -0400

    Style adjust

--- js/custom.js
+++ js/custom.js
@@ -27,9 +27,9 @@
                 distance = countDown - now;
   
           document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
   
           //do something later when date is reached
           if (distance < 0) {
@@ -46,14 +46,11 @@
 // Google Maps Iframe
 let iframe_map = document.getElementById("iframe");
 if ($(window).width() < 720) {
-  
-  
   iframe_map.innerHTML += `<div class="opacity-8 gmap_canvas"><iframe width="250" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=La%20Zucca%20Eventos&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://embedgooglemap.net/124/"></a><br><style>.mapouter{position:relative;text-align:right;height:250px;width:250px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:250px;width:250px;}</style></div>`;
 }
 else {
   iframe_map.innerHTML += `<div class="opacity-8 gmap_canvas"><iframe width="700" height="300" id="gmap_canvas" src="https://maps.google.com/maps?q=Endere%C3%A7o:%20Av.%20Tr%C3%AAs%20Barras,%20968%20-%20Vila%20Vilas%20Boas,%20Campo%20Grande%20-%20MS,%2079051-290&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:relative;text-align:right;height:300px;width:700px;}</style><a href="https://www.embedgooglemap.net">google map codes for website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:300px;width:700px;}</style></div>`;
 }
-
 
 //Music 
 var music= document.getElementById("music");
